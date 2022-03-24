dc085b2f018eeea08464ac985129909beb5ce490
Nolan Dipboye <noldipboye@leisdstudent.ws>
2022-03-24 10:55:38 -0500

    Done

--- app.js
+++ app.js
@@ -8,30 +8,34 @@ function question(a) {
   return number
 }
 
-question(askname())
-
-function mathC(b) {
+function mathC(b) { // f to c
   var C = ((b-32)*5/9);
   return C
 }
 
-function mathF(c) {
+function mathF(c) { // c to f
   var F = ((c*9)/5+32);
   return F
 }
 
-function unit(x, y) {
-var that = prompt("Which unit of temperature do you wish to convert to(Press f for farenheit and c for celsius)?")
-  if (that == "f"){
-    alert(`The temperature is ${x} degrees celsius`)
-  }
-  else if (that == "c") {
-    alert(`The temperature is ${y} degrees farenheit`)
-  }
-return that
+function choose() {
+  var that = prompt("Which unit of temperature do you wish to convert to(Press f for farenheit and c for celsius)?")
+  return that
 }
 
-unit(mathC(question()))
+function unit() {
+  var t = choose()
+  if (t == "f") {
+    alert(mathC(question(askname())))
+  } else if (t== "c") {
+    alert(mathF(question(askname())))
+  } else {
+    alert("Exit")
+  }
+}
+
+
+unit()
 
 
 
